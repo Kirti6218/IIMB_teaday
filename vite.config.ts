899e0d5bacf07d789250b1a6b32f88d911630b9f
Kirti6218 <kirtimohanty1826@gmail.com>
2025-05-07 21:29:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/teaDay/" : "/", 
+  base: mode === "production" ? "/IIMB_teaday/" : "/", 
   server: {
     host: "::",
     port: 8080,
